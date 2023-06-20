a8d00a18500a4c3af458e020abdc3ad910213612
Erick Mercado Hernández <65534702+miralis221@users.noreply.github.com>
2023-06-20 20:31:17 +0200

    UML update

--- TBRPG-Project-Alpha/Assets/_Scripts/CameraManager.cs
+++ TBRPG-Project-Alpha/Assets/_Scripts/CameraManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour
+public class Ona : MonoBehaviour
 {
     [SerializeField] private GameObject actionCameraManager;
     private void Start()
